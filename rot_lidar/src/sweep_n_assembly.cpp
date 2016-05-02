592a01f22ed6fccfe516850c2a71306a71f11580
jmpechem <jmpechem@snu.ac.kr>
2016-05-02 15:58:26 +0900

    lidar rotation upgraded

--- rot_lidar/src/sweep_n_assembly.cpp
+++ rot_lidar/src/sweep_n_assembly.cpp
@@ -15,16 +15,65 @@
 #include <tf/transform_broadcaster.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <tf2_sensor_msgs/tf2_sensor_msgs.h>
+#include <std_msgs/Bool.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/PCLPointCloud2.h>
 
+#define winding 	-1.57
+#define unwinding 	1.57
 class SNA{	// sweep and assembly class
 	public:
 	SNA();
-
+	void cloud_cb(const sensor_msgs::PointCloud2::ConstPtr& cloud);
+	void dxl_status_cb(const dynamixel_msgs::JointState::ConstPtr& msg);
+	bool is_moving_;
 	private:
-}
+	ros::NodeHandle nh_;
+	ros::Subscriber cloud_; 
+	ros::Subscriber dxl_status_;
+	ros::Publisher  assembled_cloud_pub_;
+	sensor_msgs::PointCloud2 input_;
+	pcl::PointCloud<pcl::PointXYZ> assembly_;
+	sensor_msgs::PointCloud2 output_;
+};
 SNA::SNA(){
-
+	is_moving_ = false;
+	cloud_ = nh_.subscribe<sensor_msgs::PointCloud2>("/cloud",100,&SNA::cloud_cb,this);
+	dxl_status_ = nh_.subscribe("/tilt_controller/state",100,&SNA::dxl_status_cb,this);
+	assembled_cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/assembled_cloud",100,false);
 }
+void SNA::dxl_status_cb(const dynamixel_msgs::JointState::ConstPtr& msg)
+{
+	is_moving_ = msg->is_moving;//current_pos;
+}
+void SNA::cloud_cb(const sensor_msgs::PointCloud2::ConstPtr& cloud)
+{
+	pcl::PCLPointCloud2 pcl_pc2;
+	pcl_conversions::toPCL(*cloud,pcl_pc2);
+	pcl::PointCloud<pcl::PointXYZ> buffer_cloud;
+	pcl::fromPCLPointCloud2(pcl_pc2,buffer_cloud);
+	pcl::PointXYZ temp;
+	
+	for(size_t i=0;i<buffer_cloud.size();i++)
+	{	
+		temp.x = buffer_cloud.points[i].x;
+		temp.y = buffer_cloud.points[i].y;
+		temp.z = buffer_cloud.points[i].z;		
+		assembly_.push_back(temp);	
+	}
+	pcl::toROSMsg(assembly_,output_);
+	output_.header.frame_id = "/base";	
+	assembled_cloud_pub_.publish(output_);
+	if(!is_moving_)
+	{			
+		assembly_.clear();
+	}
+	else
+	{
+
+	}
+}
+
 
 int main(int argc, char** argv)
 {
