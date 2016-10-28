4ba85520c13aaaa1ca1014ead69aaac46a335700
jmpechem <jmpechem@snu.ac.kr>
2016-10-28 17:22:55 +0900

    online lidar

--- jm_map/src/ground_filter.cpp
+++ jm_map/src/ground_filter.cpp
@@ -60,10 +60,24 @@ void Ground_Filter::GF_cb(const sensor_msgs::PointCloud2::ConstPtr& cloud){
 	
 	
 }*/
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+bool _updated = false;
+
+void Ground_Filter_cb(const sensor_msgs::PointCloud2::ConstPtr& input){
+    cloud->clear();
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl_conversions::toPCL(*input,pcl_pc2);
+    pcl::fromPCLPointCloud2(pcl_pc2,*cloud);
+    _updated = true;
+}
+
 int main(int argc, char** argv)
 {  
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-  if (pcl::io::loadPCDFile<pcl::PointXYZ> ("/home/jimin/catkin_ws/src/jm_map/pcd_data_set/3.pcd", *cloud) == -1) //* load the file
+////////////////////////////////////////////////////
+  // using pcd data format code
+////////////////////////////////////////////////////
+/*  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  if (pcl::io::loadPCDFile<pcl::PointXYZ> ("/home/jimin/catkin_ws/src/jm_map/pcd_data_set/4.pcd", *cloud) == -1) //* load the file
   {
     PCL_ERROR ("Couldn't read pcd file\n");
     return (-1);
@@ -138,6 +152,85 @@ int main(int argc, char** argv)
     ros::Time time = ros::Time::now();
     pub.publish(output_);
     rate.sleep();
+  }*/
+  ros::init(argc, argv, "ground_filter");
+  ros::NodeHandle nh;
+  ros::Publisher pub = nh.advertise<sensor_msgs::PointCloud2>("gf_cloud",100,false);
+  ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2>("shit",100,Ground_Filter_cb);
+
+  ros::Rate rate(30.0);
+  while (nh.ok()) {
+
+      if(_updated){
+
+          pcl::NormalEstimation<pcl::PointXYZ,pcl::Normal> normalEstimation;
+          normalEstimation.setInputCloud(cloud);
+          normalEstimation.setKSearch(12);
+          pcl::search::KdTree<pcl::PointXYZ>::Ptr kdtree(new pcl::search::KdTree<pcl::PointXYZ>);
+          normalEstimation.setSearchMethod(kdtree);
+          pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
+          normalEstimation.compute(*normals);
+
+          double nx = 0.0f;
+          double ny = 0.0f;
+          double nz = 0.0f;
+          double scale = 0.0f;
+          double r;
+          r = sin(deg2rad(SLOPE_THRES));
+          // Normal filtered from ground
+          pcl::PointCloud<pcl::PointXYZ>::Ptr assembly_ (new pcl::PointCloud<pcl::PointXYZ>);
+          pcl::PointXYZ temp;
+          for(int i=0;i<normals->size();i++)
+          {
+             nx = normals->at(i).normal[0];
+             ny = normals->at(i).normal[1];
+             nz = normals->at(i).normal[2];
+             scale = sqrt((nx*nx)+(ny*ny));
+             if (scale < r){
+                   if(nz > 0){
+                         temp.x = cloud->points[i].x;
+                         temp.y = cloud->points[i].y;
+                         temp.z = cloud->points[i].z;
+                         assembly_->push_back(temp);
+                              }
+                           }
+               }
+          // Step filtered from ground
+          pcl::PointXYZ filter_min;
+          pcl::PointXYZ filter_max;
+          pcl::getMinMax3D(*assembly_,filter_min,filter_max);
+          double z_thres = 0.3;
+          double z_support = 0.0f;
+          for(int i=0;i<assembly_->size();i++)
+          {
+               if (filter_min.x == assembly_->points[i].x)
+               {
+                       z_support = assembly_->points[i].z;
+               }
+          }
+               pcl::PointCloud<pcl::PointXYZ> final_assembly_;
+               for(int i=0;i<assembly_->size();i++)
+               {
+                       if ( (abs(z_support)-abs(assembly_->points[i].z)) <= z_thres)
+                       {
+                               temp.x = assembly_->points[i].x;
+                               temp.y = assembly_->points[i].y;
+                               temp.z = assembly_->points[i].z;
+                               final_assembly_.push_back(temp);
+                       }
+               }
+               sensor_msgs::PointCloud2 output_;
+               pcl::toROSMsg(final_assembly_,output_);
+               output_.header.frame_id = "base_link";
+               ros::Time time = ros::Time::now();
+               pub.publish(output_);
+               _updated = false;
+        }
+
+
+    ros::spinOnce();
+    rate.sleep();
   }
+
   return 0;
 }
